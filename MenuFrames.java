791b6a00191a46f938ed448d901e95526fa7f2bd
itschusa <itschusa@gmail.com>
2014-05-13 22:05:53 -0400

    All components visible, some functionality.
    Current features: File menu (Main Menu, Close), Help menu (About,
    Features, Shortcuts), Play, Instructions, Highscores, Easy, Medium,
    Difficult.
    Functional features: Main Menu, Close, Play.
    Notes: Possible redundancy in usage of validate, revalidate and paint.

--- MenuFrames.java
+++ MenuFrames.java
@@ -5,25 +5,37 @@ import java.awt.event.*;
 /**
  * @author Chusa Nguyen
  * @version 1.0, May 12 2014. (No functionality. Buttons do not appear.)
+ * @version 1.1, May 13 2014. (Completed framework. ActionListener only implemented to close, main menu, and play.)
  */
 
-public class MenuFrames extends JPanel //implements ActionListener
+public class MenuFrames extends JFrame
 {
-  JFrame frame = new JFrame ("Elemental Shoot-Out: A Chemistry Game");
+  //JFrame frame = new JFrame ("Elemental Shoot-Out: A Chemistry Game");
+  
+  JPanel panel1 = new JPanel();
+  JPanel panel2 = new JPanel();
+  JButton playButton = new JButton ("Play");
+  JButton instructionsButton = new JButton ("Instructions");
+  JButton scoresButton = new JButton ("High Scores");
+  JButton easyButton = new JButton ("Easy");
+  JButton mediumButton = new JButton ("Medium");
+  JButton difficultButton = new JButton ("Difficult");
+  private boolean secondPanel;
   
   public MenuFrames()
   {
+    super ("Elemental Shoot-Out: A Chemistry Game");
     menuBars();
-    //initialPanel();
+    initialPanel();
     frameSpecifications();
   }
   
   public void frameSpecifications ()
   {    
-    frame.setDefaultCloseOperation (JFrame.DISPOSE_ON_CLOSE);
-    frame.setVisible (true);
-    frame.setSize (400,300);
-    frame.setResizable (false);
+    setDefaultCloseOperation (JFrame.DISPOSE_ON_CLOSE);
+    setVisible (true);
+    setSize (400,300);
+    setResizable (false);
   }
   
   public void menuBars()
@@ -31,34 +43,88 @@ public class MenuFrames extends JPanel //implements ActionListener
     JMenuBar menus = new JMenuBar();
     JMenu fileMenu = new JMenu ("File");
     JMenu helpMenu = new JMenu ("Help");
+    JMenuItem mainMenuItem = new JMenuItem ("Main Menu");
     JMenuItem closeItem = new JMenuItem("Close");
     JMenuItem aboutItem = new JMenuItem("About");
     JMenuItem featuresItem = new JMenuItem("Features");
+    JMenuItem shortcutItem = new JMenuItem ("Shortcuts");
     
     menus.add(fileMenu);
     menus.add(helpMenu); 
+    fileMenu.add(mainMenuItem);
     fileMenu.add(closeItem);
     helpMenu.add(aboutItem);
     helpMenu.add(featuresItem);
-    frame.setJMenuBar(menus);
+    helpMenu.add(shortcutItem);
+    setJMenuBar(menus);
+    
+    mainMenuItem.addActionListener (new ActionListener(){
+      public void actionPerformed (ActionEvent e)
+      {
+        if (secondPanel)
+        {
+          remove(panel2);
+          initialPanel();
+        }
+      }
+    }); 
+    closeItem.addActionListener (new ActionListener(){
+      public void actionPerformed (ActionEvent e)
+      {
+        System.exit(0);
+      }
+    }); 
   }
   
   public void initialPanel()
   {
-    JPanel panel = new JPanel();
-    panel.setLayout (new GridBagLayout());
+    secondPanel = false;
+    panel1.setLayout (new GridBagLayout());
     GridBagConstraints constraints = new GridBagConstraints();
-    JButton playButton = new JButton ("Play");
-    JButton instructionsButton = new JButton ("Instructions");
-    JButton scoresButton = new JButton ("High Scores");
     
+    constraints.fill=constraints.HORIZONTAL;
+    constraints.weighty = 1;
     constraints.gridx = 1;
     constraints.gridy = 1;
-    panel.add(playButton, constraints);
+    panel1.add(playButton, constraints);
+    constraints.gridy = 2;
+    panel1.add(instructionsButton, constraints);
+    constraints.gridy = 3;
+    panel1.add(scoresButton, constraints);
+    
+    add(panel1);
+    repaint();
+    
+    playButton.addActionListener (new ActionListener ()
+                                    {
+      public void actionPerformed (ActionEvent e)
+      { 
+        remove(panel1);
+        levelsPanel();
+        repaint();
+      }
+    });
   }
   
   public void levelsPanel()
   {
+    secondPanel = true;
+    panel2.setLayout (new GridBagLayout());
+    GridBagConstraints constraints = new GridBagConstraints();
+    
+    
+    constraints.fill=constraints.HORIZONTAL;
+    constraints.weighty = 1;
+    constraints.gridx = 1;
+    constraints.gridy = 1;
+    panel2.add(easyButton, constraints);
+    constraints.gridy = 2;
+    panel2.add(mediumButton, constraints);
+    constraints.gridy = 3;
+    panel2.add(difficultButton, constraints);
+  
+    add(panel2);
+    revalidate();
   }
   
   public static void main(String [] args)
